6c6a1be46e20acacac152b74f3dc5557a6229e32
Simon Guest <me@simonguest.com>
2023-08-12 09:41:31 -0700

    Another build with branch protection

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ let app = document.getElementById("app")
 async function init() {
   console.log("Running")
   let textNode = document.createElement("p")
-  textNode.innerText = "Build was successful"
+  textNode.innerText = "Build was successful!"
   app?.appendChild(textNode)
 }
 
