7c9cac82ca4be6f4686e99920507959f234fcd82
Simon Guest <me@simonguest.com>
2023-08-12 16:09:22 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let app = document.getElementById("app")
+
+async function init() {
+  console.log("Running")
+  let textNode = document.createElement("p")
+  textNode.innerText = "Build was successful"
+  app?.appendChild(textNode)
+}
+
+init()
