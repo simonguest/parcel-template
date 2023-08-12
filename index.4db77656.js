4da7e02027a59fe1adfd6ffa7ffdb418176f2d0a
simonguest <simonguest@users.noreply.github.com>
2023-08-12 16:38:17 +0000

    deploy: bdc8d3a2e6f51248412cce82fa39304dcc780494

---
+++ index.4db77656.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("app");!async function(){console.log("Running");let n=document.createElement("p");n.innerText="Build was successful",e?.appendChild(n)}();
+//# sourceMappingURL=index.4db77656.js.map
