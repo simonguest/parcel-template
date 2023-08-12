1993af4e82790f5328e706beed6e61fd5aabf7ea
simonguest <simonguest@users.noreply.github.com>
2023-08-12 16:41:56 +0000

    deploy: 6c6a1be46e20acacac152b74f3dc5557a6229e32

---
+++ index.f6cb292f.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("app");!async function(){console.log("Running");let n=document.createElement("p");n.innerText="Build was successful!",e?.appendChild(n)}();
+//# sourceMappingURL=index.f6cb292f.js.map
